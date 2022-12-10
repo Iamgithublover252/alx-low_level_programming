8eddb278cd58817382870df87920c03d78177f97
Iamgithublover252 <inanuur252@gmail.com>
2022-12-10 03:09:10 -0800

    5-printf.c Task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-	Printf("\"with proper grammar, but the outcome is a piece of art,");
+	Printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
