df9f62b7f354431ab37b6bb39e4d11c0728e5c9d
Iamgithublover252 <inanuur252@gmail.com>
2022-12-12 11:26:40 -0800

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
+        char low;
 	char low;
 
+        for (low = 'z'; low >= 'a'; low--)
+                putchar(low);
+        putchar('\n');
 	for (low = 'z'; low >= 'a'; low--)
 		putchar(low);
 		putchar('\n');
 
+        return (0);
 	return (0);
 }
+
